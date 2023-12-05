5fa52e6dfc764767cb49397dd5f6b53ff503acf0
Borja <amanecerbo2@gmail.com>
2023-12-05 13:11:21 +0100

    Diseño activity_agregar_nota

---
+++ app/src/main/java/com/bgallego/agenda_online/AgregarNota/Agregar_Nota.java
@@ -0,0 +1,16 @@
+package com.bgallego.agenda_online.AgregarNota;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bgallego.agenda_online.R;
+
+public class Agregar_Nota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregar_nota);
+    }
+}
